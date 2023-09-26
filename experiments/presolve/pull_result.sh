5c9b6afe54e1a8552651ad7b7a1135b2da1fde8d
Renato Farruggio <renato.farruggio@unibas.ch>
2023-09-26 18:18:35 +0200

    Add shell script to pull results from experiment.

---
+++ experiments/presolve/pull_result.sh
@@ -0,0 +1,6 @@
+
+if [ ! -d "data" ]; then
+  mkdir data
+fi
+
+scp farren00@login-infai.scicore.unibas.ch:/infai/farren00/workspace/downward-projects/downward/experiments/presolve/data/2023-09-26-presolve-eval/2023-09-26-presolve-abs.html /home/bob/workspace/downward-projects/downward/experiments/presolve/data
